131899bde19c035d12d4a92bc5548d4d45eceb9e
JonatanV <nofettatjarn1>
2019-04-04 15:03:29 +0200

    second

--- js/game.js
+++ js/game.js
@@ -1,32 +1,49 @@
-console.log("Hello World");
+console.log("HEllo World");
 
 let gameButton = document.getElementById("game");
+let bankElement = document.getElementById("bank");
+let powerupsDiv  = document.getElementById("powerups");
 
-let banKElement = document.getElementById("bank")
-
-let powerUpsDiv = document.getElementById("powerups");
-
-let bank=0;
-let pupCost=10;
+let bank = 0;
+let pupCost = 10;
+let pupCost2 = 20;
+let clickValue = 1;
 
 gameButton.addEventListener('click',function(){
-    //console.log("click")
-    bank +=1;
-    gameButton.textContent=Math.floor(bank);
-})
-
-let powerUp = document.createElement("button");
-powerUp.textContent = "Unlimited Power" 
-powerUp.addEventListener('click',function(){
-    if (bank >= pupCost){
-        bank= bank - pupCost;
-        pupCost = pupCost*1,5;
-        bank = bank * 10;
-        banKElement.textContent = "Du köpte en PowerUp"
-    }else {
-        console.log("Du har inte råd!");
-        banKElement.textContent= "Du har inte råd"
-    }
+    bank+=clickValue;
+    gameButton.textContent = Math.floor(bank);
 });
 
-powerUpsDiv.appendChild(powerUp);
+let powerups = document.createElement("button");
+    powerups.textContent = "Great "+pupCost;
+    powerups.addEventListener('click', function(){
+        if (bank >= pupCost){
+            bank = bank-pupCost;
+            pupCost = pupCost*7;
+            bank = bank*10;
+            powerups.textContent = "Great " + pupCost;
+            bankElement.textContent = "Du köpte en powerUp!"
+        } else{
+            bankElement.textContent = "Du har inte råd!" 
+        }
+    });
+ 
+    powerupsDiv.appendChild(powerups);
+
+    let powerup = document.createElement("button");
+    powerup.textContent = "sick "+ pupCost2;
+    powerup.addEventListener('click', function(){
+        if (bank >= pupCost2){
+            bank = bank-pupCost2;
+            pupCost2 = pupCost2*7;
+            clickValue=clickValue*2;
+            powerup.textContent = "Sick  " + pupCost2;
+            bankElement.textContent = "Du köpte en powerUp!"
+        } else{
+            bankElement.textContent = "Du har inte råd!" 
+        }
+    });
+ 
+    powerupsDiv.appendChild(powerup);
+    
+    
