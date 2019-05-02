f880c96d4d361391697609b624f07eb46854d26d
JonatanV <nofettatjarn1>
2019-05-02 15:20:48 +0200

    färdig

--- js/game.js
+++ js/game.js
@@ -8,11 +8,14 @@ let bank = 0;
 let powerupvalue = 0;
 let pupCost = 10;
 let pupCost2 = 20;
-let clickValue = 1;
+let clickValue1=1;
+let clickValue = 0;
+let clickValue2 = 0;
 let pupCost3 = 200;
+let pupCost4 = 1000;
 
 gameButton.addEventListener('click',function(){
-    bank+=clickValue;
+    bank+=clickValue1;
     gameButton.textContent = Math.floor(bank);
 });
 
@@ -21,7 +24,7 @@ powerup.textContent = "Great "+pupCost;
 powerup.addEventListener('click', function(){
     if (bank >= pupCost){
         bank = bank-pupCost;
-        pupCost = pupCost*7;
+        pupCost = pupCost*12;
         bank = bank*10;
         powerup.textContent = "Great " + pupCost;
         bankElement.textContent = "Du köpte en powerUp!"
@@ -53,10 +56,13 @@ powerupsDiv.appendChild(powerup);
         powerup3.addEventListener('click', () => {
             if (bank >= pupCost3) {
                 bank -= pupCost3;
-                powerupvalue+=0.3;
+                clickValue++;
+                powerupvalue+=0.3*clickValue;
                 pupCost3 = pupCost3 * 10;
                 powerup3.textContent= "ting "+pupCost3;
-
+                bankElement.textContent = "Du köpte en powerUp!"
+            } else{
+                bankElement.textContent = "Du har inte råd!" 
             }
         });
     
@@ -69,3 +75,28 @@ powerupsDiv.appendChild(powerup);
     window.requestAnimationFrame(step);
     
     powerupsDiv.appendChild(powerup3);
+
+    let powerup4 = document.createElement("button");
+        powerup4.textContent= "tang " + pupCost4;   
+        powerup4.addEventListener('click', () => {
+            if (bank >= pupCost4){ 
+                bank -= pupCost4;
+                clickValue++;
+                powerupvalue+=0.6 * clickValue2;
+                pupCost4 = pupCost4 * 11 * clickValue;
+                powerup4.textContent= "ting "+pupCost4;
+                bankElement.textContent = "Du köpte en powerUp!"
+            } else{
+                bankElement.textContent = "Du har inte råd!" 
+            }
+        });
+    
+    function step() {
+        bank += powerupvalue;
+        game.textContent = Math.floor(bank);
+        window.requestAnimationFrame(step);
+    }
+    
+    window.requestAnimationFrame(step);
+    
+    powerupsDiv.appendChild(powerup4);
